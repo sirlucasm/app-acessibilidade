ed0b2e190ede6df1fd80260c412ecd70818222ae
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-09-13 02:20:07 -0300

    feat: added new normalize (responsivity)
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ src/utils/normalize.ts
@@ -0,0 +1,57 @@
+import { Dimensions } from 'react-native';
+import { PixelRatio } from 'react-native';
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const widthBaseScale = SCREEN_WIDTH / 414;
+const heightBaseScale = SCREEN_HEIGHT / 896;
+
+function normalize(size: number, based: 'width' | 'height' = 'width') {
+  const newSize = (based === 'height') ? size * heightBaseScale : size * widthBaseScale;
+  return Math.round(PixelRatio.roundToNearestPixel(newSize));
+}
+
+/**
+ * for width pixel
+ * @param size number
+ * @returns number
+ */
+export const widthPixel = (size: number) => {
+  return normalize(size, 'width');
+};
+
+/**
+ * for height pixel
+ * @param size number
+ * @returns number
+ */
+export const heightPixel = (size: number) => {
+  return normalize(size, 'height');
+};
+
+/**
+ * for font pixel
+ * @param size number
+ * @returns number
+ */
+export const fontPixel = (size: number) => {
+  return heightPixel(size);
+};
+
+/**
+ * for Margin and Padding vertical pixel
+ * @param size number
+ * @returns number
+ */
+export const pixelSizeVertical = (size: number) => {
+  return heightPixel(size);
+};
+
+/**
+ * for Margin and Padding horizontal pixel
+ * @param size number
+ * @returns number
+ */
+export const pixelSizeHorizontal = (size: number) => {
+  return widthPixel(size);
+};
