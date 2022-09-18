ecabb22e8e1163528c3ad54a7c89d168c6a7ad91
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-09-18 20:33:36 -0300

    feat: added firebase settings and environment variables
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ src/configs/firebase.ts
@@ -0,0 +1,34 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
+
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+}
+
+let app;
+if (!getApps().length) app = initializeApp(config);
+else app = getApp();
+
+export const firestore = initializeFirestore(app, { experimentalForceLongPolling: true });
+
+// setLogLevel('debug');
+
+export const auth = getAuth();
+export const storage = getStorage();
