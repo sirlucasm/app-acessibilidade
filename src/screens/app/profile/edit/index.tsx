885e22a872fde55105b2af63c68c8359c7758e00
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-10-30 20:48:54 -0300

    feat: added profil pic upload
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/screens/app/profile/edit/index.tsx
+++ src/screens/app/profile/edit/index.tsx
@@ -1,5 +1,5 @@
 import { ProfileHeader } from "@components/TopHeader/profileHeader";
-import { Avatar, ScrollView, VStack } from 'native-base';
+import { Avatar, Box, ScrollView, View, VStack } from 'native-base';
 import { DeficienciesInfo } from '../styles';
 import useAuthContext from "src/contexts/auth-context/use-auth-context";
 import { useUser } from "src/hooks/use-user";
@@ -7,11 +7,41 @@ import { getUserNameLetters } from 'src/utils/user';
 import { TERTIARY } from '@styles/colors';
 import AppLoading from '@components/AppLoading';
 import { EditProfileForm } from "./form";
+import { storage } from 'src/configs/firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { Octicons } from "@expo/vector-icons";
 
 
 const EditProfile = () => {
   const { user, deficiency } = useUser();
-  const { currentUser } = useAuthContext();
+  const { currentUser }: any = useAuthContext();
+
+  const handleChooseProfileImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 0.8,
+    });
+
+    if (!result.cancelled) {
+      handleUploadProfileImage(result.uri);
+    }
+  }
+
+  const handleUploadProfileImage = async (imageFile: string) => {
+    const image = await fetch(imageFile);
+    const imageBlob = await image.blob();
+    const storageRef = ref(storage, `images/profpics/${Date.now()}_${user.email}`);
+    const snapshot = await uploadBytes(storageRef, imageBlob);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    await updateProfile(currentUser, {
+      photoURL: downloadURL,
+    });
+  }
 
   if (!user || !deficiency) return <AppLoading />;
 
@@ -19,15 +49,29 @@ const EditProfile = () => {
     <>
       <ProfileHeader editProfile />
       <VStack alignItems='center' bottom='16' margin={0}>
-        <Avatar
-          size="2xl"
-          bg="blue.500"
-          source={{ uri: currentUser?.photoURL || 'https://bit.ly/broken-link' }}
-          borderColor={TERTIARY}
-          borderWidth={12}
-        >
-          {getUserNameLetters(currentUser?.displayName || '')}
-        </Avatar>
+        <Box position='relative'>
+          <Avatar
+            size="2xl"
+            bg="blue.500"
+            source={{ uri: currentUser?.photoURL || 'https://bit.ly/broken-link' }}
+            borderColor={TERTIARY}
+            borderWidth={12}
+          >
+            {getUserNameLetters(currentUser?.displayName || '')}
+          </Avatar>
+          <View
+            position='absolute'
+            right={4}
+            bottom={4}
+            backgroundColor={TERTIARY}
+            borderRadius={5}
+            padding={1}
+          >
+            <TouchableOpacity onPress={handleChooseProfileImage}>
+              <Octicons name='pencil' size={17} />
+            </TouchableOpacity>
+          </View>
+        </Box>
       </VStack>
       <DeficienciesInfo>
         <ScrollView bottom='10' w={["280", "300"]}>
